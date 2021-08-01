8b20590cc7a4e9e1dddbd4dee8ab886bd3b2a82b
jihoon <akdlsz21@gmail.com>
2021-08-01 17:00:35 +0900

    map related function not added, refer to comment in contact.js

--- src/contact.js
+++ src/contact.js
@@ -6,3 +6,7 @@ export default function contact() {
 	map.classList.add('map-active');
 	content.append(map);
 }
+
+// Cant find way to deactivate maps when contact is not active.
+// Maps should only pop up when contact-us tab is clicked on.
+// maps.innerHTML doesn't recognize script from kakao-map.
